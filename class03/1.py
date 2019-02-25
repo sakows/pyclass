88127f972b579954ab041a099d222d174a0edecd
Sako Mammadov <sako@opengov.com>
2019-02-24 19:58:19 -0800

    Added Class03

---
+++ class03/1.py
@@ -0,0 +1,40 @@
+# print('My name is')
+# for i in range(5):
+    # print('elgun',i)
+    # print('letsdo it')
+# 
+
+# total=0
+# for i in range(11):
+#     total=total+i
+#     print('Value of i: ',i)
+#     print('Value of total: ',total)
+# print(total)
+
+# i=0
+# while i<6:
+#     print('The value of i:'+str(i))
+#     i+=2
+
+# Stat=4, End=8, Step=2
+# for i in range(4,8,2):
+#     print(i)
+
+# for i in range(8,-4,-1):
+#     print(i)
+
+# import random
+# for i in range(5):
+#     print(random.randint(1,10))
+
+import sys
+while True:
+    print('Type exit')
+    resp=input()
+    if resp=='exit':
+        sys.exit()
+        #break
+
+print('Sako is running')
+
+
