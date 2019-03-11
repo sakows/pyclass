f580c12bbba8d04f86d0a2a4a6d531bbefe89400
Sako Mammadov <sako@opengov.com>
2019-03-10 18:47:26 -0700

    Added Class04

---
+++ class04/1.py
@@ -0,0 +1,69 @@
+# def h():
+#     print('Hello W')
+
+# h()
+#//
+
+# def h(name):
+#     #print('Hello',name)
+#     r_name=name
+#     return r_name
+
+
+# g_name=h('Elgun')
+# print(g_name)
+
+
+# import random
+# import sys
+
+# def h2(number):
+#     if number==1:
+#         print('you got it')
+#     elif number==2:
+#         print('u didnt get it')
+#     else:
+#         print('game_over')
+#         sys.exit()
+
+# r=random.randint(1,9)
+# h2(r)
+
+#print('Hello World','World',sep='--')
+
+# a1=4
+# a2=20
+# a3=60
+# def f1():
+#     a2=5
+#     a1=40
+#     total=a2+a1
+#     global a3
+#     a3=60
+#     return a3
+
+
+# def f2():
+#     a=f1()
+#     a4=a3+45
+#     return a4
+
+
+# b=f2()
+# print('the value came from f2 is',b)
+
+def spam(n):
+    # try:
+    #     return 43/n
+    # except ZeroDivisionError:
+    #     print('Dont put 0 in mexrec')
+    return 44/n
+
+spam(12)
+spam(0)
+print('hello')
+
+
+
+
+
